a53aa49dd4e50e042c118d3c141311be1b31395c
muchira007 <muchirasteve2@gmail.com>
2024-02-19 05:10:14 -0800

    error handling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,22 +23,22 @@ void swap_ints(int *a, int *b)
 void bubble_Sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	bool bubbly = true;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (bubbly)
 	{
-		bubbly = true;
+		bubbly = false;
 
 		for(i = 0; i < len - 1 ; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				swap_ints(&array[i], &array[i + 1]);
 				print_array(array, size);
-				bubbly = false;
+				bubbly = true;
 			}
 		}
 		len--;
