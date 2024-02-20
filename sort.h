efd6859e1d4500f2b0f4e4daa207a2f561adcffd
muchira007 <muchirasteve2@gmail.com>
2024-02-20 14:31:20 -0800

    fixed error added insertion

--- sort.h
+++ sort.h
@@ -1,18 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
-/**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
@@ -25,10 +17,28 @@ typedef enum bool
 } bool;
 
 /**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/**
  * Protorpyes for default functions
  */
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
-void bubble_Sort(int *array, size_t size
+void print_array(const int *array, size_t size);
+void bubble_Sort(int *array, size_t size);
+void swap_n(listint_t **h, listint_t **n1, listint_t *n2);
+void insertion_sort_list(listint_t **list);
+
 #endif
