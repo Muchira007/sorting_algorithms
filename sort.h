2f16b70cb683fc72b0710f6fe9acea5c01923860
muchira007 <muchirasteve2@gmail.com>
2024-02-20 15:13:23 -0800

    final tasks

--- sort.h
+++ sort.h
@@ -37,5 +37,7 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void swap_n(listint_t **h, listint_t **n1, listint_t *n2);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
